3cf83d58e240cc6bc2087a8ca9496b5101f234be
Caroline Chen <carolinetychen@gmail.com>
2016-03-24 14:26:45 +0800

    random select pixels

--- main.m
+++ main.m
@@ -1,10 +1,10 @@
 j = 13;
 
-Img = {};
+Images = {};
 Exposure = zeros(j,1);
 Y_Image = {};
 for i = 1:j
-    ImagePath = sprintf('/Users/Daphne/Desktop/vfx_hw1_HDR/exposures/img%02d.jpg', i);
+    ImagePath = sprintf('exposures/img%02d.jpg', i);
     img = imread(ImagePath);
     Images{i} = img; 
    	info = imfinfo(ImagePath);
@@ -12,9 +12,24 @@ for i = 1:j
     disp(ImagePath);
     imshow(Images{i});
 end
-disp(Exposures);
+
+[imgRow, imgCol, channel]= size(Images{1});
+disp(imgRow);
+disp(imgCol);
+disp(channel);
+
+% Select 200 pixels randomly
+rdm = randi([1, 768 * 1024], 1, 200);
+disp(rdm);
+
+% filling z matrix
+zimage = zeros(200, j, channel);
+
+
+%disp(Exposures);
 ln_t = log(Exposures);
-disp(ln_t);
+%disp(ln_t);
 
+% random select pixels
 
-%[g, lE] = gsolve(Z, B, l, w)
+% [g, lE] = gsolve(Z, B, l, w)
