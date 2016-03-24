60e35dcfc39eb026154d1bb9c6021f7fa0d60fab
AChin1311 <daphne011552000@yahoo.com.tw>
2016-03-24 15:37:48 +0800

    add HDR_Map.m modify main.m

--- HDR_Map.m
+++ HDR_Map.m
@@ -1,3 +1,18 @@
-function HDR_Map(g, lE, w, j)
-  
+function HDR_img = HDR_Map(images, g, lE, ln_t, w, number)
+    length = size(images{1});
+    %disp(images{1});
+    lE = zeros(length);
+    
+    for pixel = 1:length
+        sum_w = 0;
+        for j = 1:number
+            Zij = images{j}(pixel); 
+            lE(pixel) = lE(pixel) + w(Zij+1)*(g(Zij+1)-ln_t(j));
+            sum_w = sum_w + w(Zij+1);
+        end
+        lE(pixel) = lE(pixel)/sum_w;
+    end
+    HDR_img = exp(lE);
+    index = find(isnan(HDR_img) | isinf(HDR_img));
+    HDR_img(index) = 0;
 end
