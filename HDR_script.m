6efd932af4aa96e4766dabf6284501d650af0807
Caroline Chen <carolinetychen@gmail.com>
2016-03-24 13:38:29 +0800

    add gsolve function

--- HDR_script.m
+++ HDR_script.m
@@ -4,7 +4,7 @@ Img = {};
 Exposure = zeros(j,1);
 Y_Image = {};
 for i = 1:j
-    ImagePath = sprintf('/Users/Daphne/Desktop/NTU/大三下/數位視覺效果/HDR_hw/exposures/img%02d.jpg', i);
+    ImagePath = sprintf('/Users/carolinechen/Desktop/exposures/img%02d.jpg', i);
     img = imread(ImagePath);
     Images{i} = img; 
    	info = imfinfo(ImagePath);
@@ -12,14 +12,19 @@ for i = 1:j
     disp(ImagePath);
     imshow(Images{i});
     
-    disp(size(Images{i}));
+    % disp(size(Images{i}));
     imageSize = size(Images{i});
     YCBCR = rgb2ycbcr(Images{i});
     Y_Image{i} = YCBCR(1:imageSize(1), 1:imageSize(2), 1);
     imshow(Y_Image{i});
 end
+
+imgRow = 
+
 disp(Exposures);
 ln_t = log(Exposures);
 disp(ln_t);
 
-%[g, lE] = gsolve(Z, B, l, w)
+% Solving g and lE by gsolve
+g = zeros(256, 3);
+lE = zeros(imgRow, imgCol, 3);
---
+++ gsolve.m
@@ -0,0 +1,43 @@
+function [g, lE] = gsolve(Z, B, l, w)
+	% Arguments:
+	% 	Z(i, j) is the pixel value of the pixel i in the image j
+	% 	B(j) is log delta t(exposure time) of the image j
+	% 	l is lambda, the constant that determines the amount of smoothness
+	% 	w is the weighting function for pixel value z
+
+	n = 256;
+	A = zeros(size(Z, 1) * size(Z, 2) + n + 1, n + size(Z, 1));
+	b = zeros(size(A,1),1);
+
+	% Include the data-fitting equations
+	k = 1; 
+	for i = 1 : size(Z, 1)
+		for j = 1 : size(Z, 2)
+			wij = w(Z(i, j) + 1);
+			A(k,Z(i, j) + 1) = wij;
+			A(k, n + i) = -wij;
+			b(k, 1) = wij * B(i, j);
+			k = k + 1;
+		end 
+	end
+
+	% Set the middle value to 0
+	A(k,129) = 1;
+	k = k + 1;
+
+	% Smoothness equation
+	for i = 1 : n - 2
+	  A(k, i) = l * w(i + 1);
+	  A(k,i + 1) = -2 * l * w(i + 1);
+	  A(k,i + 2) = l * w(i + 1);
+	  k = k + 1;
+	end
+
+	% Solve the system using SVD
+	x = A \ b;
+
+	% Solve g and lE
+	g = x(1 : n);
+	lE = x(n + 1 : size(x, 1));
+
+end
