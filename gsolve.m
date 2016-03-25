7819bf9e1a61e4b6325644992c463d7db60d3a28
Caroline Chen <carolinetychen@gmail.com>
2016-03-25 12:50:59 +0800

    comment on gsolve.m

--- gsolve.m
+++ gsolve.m
@@ -1,9 +1,20 @@
 function [g, lE] = gsolve(Z, B, l, w)
+	%
+	% This code is from the following paper
+	%
+	% P. E. Debevec and J. Malik (1997). Recovering High Dynamic Range Radiance Maps from
+	% Photographs. Proceedings of SIGGRAPH 1997, ACM Press / ACM SIGGRAPH, 369–378.
+	%
+	%
 	% Arguments:
-	% 	Z(i, j) is the pixel value of the pixel i in the image j
-	% 	B(j) is log delta t(exposure time) of the image j
-	% 	l is lambda, the constant that determines the amount of smoothness
-	% 	w is the weighting function for pixel value z
+	% 	Z(i, j)	is the pixel values of pixel location number i in image j
+	% 	B(j)	is the log delta t, of log shutter speed, for image j
+	% 	l 		is lamdba, the constant that determines the amount of smoothness
+	% 	w(z)	is the weighting function value for pixel value z
+	%	
+	% Returns:
+	%	g(z) is the log exposure corresponding to pixel value z
+	%	lE(i) is the log film irradiance at pixel location i
 
 	n = 256;
 	A = zeros(size(Z, 1) * size(Z, 2) + n + 1, n + size(Z, 1));
@@ -22,14 +33,14 @@ function [g, lE] = gsolve(Z, B, l, w)
 	end
 
 	% Set the middle value to 0
-	A(k,129) = 1;
+	A(k, 129) = 1;
 	k = k + 1;
 
 	% Smoothness equation
 	for i = 1 : n - 2
 	  A(k, i) = l * w(i + 1);
-	  A(k,i + 1) = -2 * l * w(i + 1);
-	  A(k,i + 2) = l * w(i + 1);
+	  A(k, i + 1) = -2 * l * w(i + 1);
+	  A(k, i + 2) = l * w(i + 1);
 	  k = k + 1;
 	end
 
