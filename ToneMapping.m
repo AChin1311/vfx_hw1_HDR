5a5077e1e66754f54f47a13c5acb70299306c0a7
Caroline Chen <carolinetychen@gmail.com>
2016-03-28 19:26:38 +0800

    different white

--- ToneMapping.m
+++ ToneMapping.m
@@ -1,11 +1,13 @@
 function tone_map_img = ToneMapping(img)
     delta = 1e-6;
     a = 0.18;
-    white = 0.5;
+    white = 2.5;
+    % white = 0.5;
     Lw = 0.2126 * img(:,:,1) + 0.7152 * img(:,:,2) + 0.0722 * img(:,:,3);
     LwMean = exp(mean(mean(log(delta + Lw))));
     Lm = (a / LwMean) * Lw;
     Ld = (Lm .* (1 + Lm / (white * white))) ./ (1 + Lm);
+    
     for channel = 1:3
         Cw = img(:,:,channel) ./ Lw;
         tone_map_img(:,:,channel) = Cw .* Ld;
